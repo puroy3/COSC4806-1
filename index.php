907681a01d3475eff24b5f5854f95f743128479e
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-01 01:21:36 +0000

    Added a failed attempt sessions and a redirect

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+
+// Check if user is authenticated.
+// If not, send them to login.php using header()...
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,7 +15,7 @@
 
       <h1>Assignment 1</h1>
 
-      <p><a href="/login.php">Click here to login</a></p>
+      <p> Welcome, <?=$_SESSION['username'] ?></p>
     
   </body>
 </html>
--- validate.php
+++ validate.php
@@ -1,16 +1,28 @@
 <?php
 
+  session_start();
+
   $valid_username = "pushpak";
   $valid_password = "1234";
 
   $username = $_REQUEST['username'];
+  $_SESSION['username'] = $username;
   $password = $_REQUEST['password'];
-
+  
   if ($valid_username == $username && $valid_password == $password) {
-    echo "Successful login";
+    header ('location: /');
   }
   else {
-    echo "Failed to login";
+    
+    if (!isset($_SESSION['failed_attempts'])){
+      $_SESSION['failed_attempts'] = 1;
+    }
+    else {
+      $_SESSION['failed_attempts'] = $_SESSION['failed_attempts'] + 1;
+    }
+
+    echo "This is unsuccessful attempt number " . $_SESSION['failed_attempts'];  
+    
   }
 
 ?>
